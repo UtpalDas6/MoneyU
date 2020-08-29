70cff4cdd7246808a253aff2c772c77970f573ea
Utpal Das <36782722+UtpalDas6@users.noreply.github.com>
2020-08-29 20:21:16 +0530

    MONGODB INTEGRATED

--- python/mongo.py
+++ python/mongo.py
@@ -1,44 +1,58 @@
+'''
+author: Utpal Das
+'''
 import pymongo
-CLIENT = pymongo.MongoClient("mongodb://localhost:27017/")
+import json
+from datetime import datetime, timedelta
 
-def create_db(db_name):
-    if not check_db_exists(db_name):
-        db_name = CLIENT[db_name]
-        print('created database:'+str(db_name.name))
-        return db_name
-    return db_name
+class MongoDB:
+    def __init__(self,url="mongodb://localhost:27017/", db_name='moneydb', col_name='moneypage'):
+        self.myclient = pymongo.MongoClient(url)
+        self.dblist = (self.myclient.list_database_names())
+        self.db_name = self.create_db(db_name)
+        self.col_name = self.create_collection(col_name)
 
-def drop_db(db_name):
-    pass
 
-def get_db_list():
-    return CLIENT.list_database_names()
+    def create_db(self,db_name):
+        if db_name not in self.dblist:
+            self.db_name = self.myclient[db_name]
+            print('Created DB : %s' % db_name)
+            return self.db_name
+        else:
+            print('%s already exists' % db_name)
+            return self.myclient[db_name]
 
-def check_db_exists(db_name):
-    if db_name in get_db_list():
-        return True
-    return False
+    def create_collection(self,col_name):
+        collist = self.db_name.list_collection_names()
+        if col_name in collist:
+            print("%s already exists." % col_name)
+            return self.db_name[col_name]
+        else:
+            self.col_name = self.db_name[col_name]
+            print('Create collection : %s',self.col_name)
+            return self.col_name
 
-def create_collection(db_name,collection_name):
-    if not check_collection_exists(db_name,collection_name):
-        collection_name=db_name[collection_name]
-        return collection_name
-    return collection_name
+    def read_transaction(self,data):
+        pk = data
+        try:
+            data = self.col_name.find({'date':pk})[0]
+        except:
+            data = {}
+        if (data):
+            return json.dumps(data, default=str)
+        else:
+            data['date'] = pk
+            data['incomes'] = []
+            data['expenses'] = []
+            return json.dumps(data, default=str)
 
-def drop_collection(db_name,collection_name):
-    pass
+    def insertorupdate_transaction(self,data):
+        data = json.loads(data)
+        pk = (datetime.strptime(data['date'][0:10],'%Y-%m-%d')+timedelta(1)).strftime('%Y-%m-%d')
+        data['date'] = pk
+        self.col_name.update({ 'date': pk },data, upsert=True )
+        data = (self.col_name.find({'date':pk})[0])
+        return json.dumps(data, default=str)
 
-def get_collection_list(db_name):
-    return db_name.list_collection_names()
-
-def check_collection_exists(db_name,collection_name):
-    if collection_name in get_collection_list(db_name):
-        return True
-    return False
-if __name__ == "__main__":
-    print('====DROP====')
-    print(get_db_list())
-    db = create_db('user')
-    print(get_collection_list(db))
-    collection = create_collection(db,'admin')
-    print(get_collection_list(db))
+    def delete_transaction(self):
+        self.col_name.drop()
